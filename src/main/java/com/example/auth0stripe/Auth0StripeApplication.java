2e484c99f35a179971364f961edd4935af5f3a28
Muhammad Ahmad Shahid <ahmadshahid856@gmail.com>
2022-06-05 18:36:55 +0500

    Initial commit

---
+++ src/main/java/com/example/auth0stripe/Auth0StripeApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth0stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0StripeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth0StripeApplication.class, args);
+    }
+
+}
